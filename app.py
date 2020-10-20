0ae989fee8a2a7ccfd3cf10d213eb85b4ac902ea
Narjiss Sallahi <72303318+SallahiNarjiss@users.noreply.github.com>
2020-10-20 21:53:19 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 import os
-from flask import Flask
-from flask_appbuilder import SQLA, AppBuilder
-
+from flask import Flask, render_template
+from flask_appbuilder import SQLA, AppBuilder, AppBuilder, expose, BaseView, has_access
 # init Flask
 app = Flask(__name__)
-
 # Basic config with security for forms and session cookie
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.db')
@@ -16,6 +14,72 @@ db = SQLA(app)
 # Init F.A.B.
 appbuilder = AppBuilder(app, db.session)
 
+
+#creating the views
+class MyView(BaseView):
+    default_view = 'method1'
+    @expose('/method1/')
+    @has_access
+    def method1(self):
+        # do something with param1
+        # and return to previous page or index
+        return 'Hello'
+    @expose('/method2/<string:param1>')
+    @has_access
+    def method2(self, param1):
+        # do something with param1
+        # and render template with param
+        param1 = 'Goodbye %s' % (param1)
+        return param1
+    @expose('/methode3/<string:param1>')
+    @has_access
+    def method3(self, param1):
+        # do something with param1
+        # and render template with param
+        param1 = 'Goodbye %s' % (param1)
+        self.update_redirect()
+        return self.render_template('methode3.html',
+                            param1 = param1)
+appbuilder.add_view(MyView, "Method1", category='My View')
+appbuilder.add_link("Method2", href='/myview/method2/john', category='My View')
+appbuilder.add_link("Methode3", href='/myview/methode3/john', category='My View')
+
+
+
+from sqlalchemy import Column, Integer, String, ForeignKey, Date
+from sqlalchemy.orm import relationship
+from flask_appbuilder import Model
+
+class ContactGroup(Model):
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), unique = True, nullable=False)
+
+    def __repr__(self):
+        return self.name
+
+    id = Column(Integer, primary_key=True)
+    name =  Column(String(150), unique = True, nullable=False)
+    address =  Column(String(564), default='Street ')
+    birthday = Column(Date)
+    personal_phone = Column(String(20))
+    personal_cellphone = Column(String(20))
+    contact_group_id = Column(Integer, ForeignKey('contact_group.id'))
+    contact_group = relationship("ContactGroup")
+
+    def __repr__(self):
+        return self.name
+
+
+from flask_appbuilder import ModelView
+from flask_appbuilder.models.sqla.interface import SQLAInterface
+
+class GroupModelView(ModelView):
+    datamodel = SQLAInterface(ContactGroup)
+    related_views = [ContactModelView]
+
+
+
+
+
 # Run the development server
 app.run(debug=True)
-
