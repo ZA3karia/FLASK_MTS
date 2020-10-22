2d82a5c85204aae45490609a37feedacb50b5b19
Narjiss Sallahi <72303318+SallahiNarjiss@users.noreply.github.com>
2020-10-22 08:13:55 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,26 @@
 import os
 from flask import Flask, render_template
 from flask_appbuilder import SQLA, AppBuilder, AppBuilder, expose, BaseView, has_access
+from flask_bootstrap import Bootstrap
+
+from wtforms import Form, StringField
+from wtforms.validators import DataRequired
+from flask_appbuilder.fieldwidgets import BS3TextFieldWidget
+from flask_appbuilder.forms import DynamicForm
+
+
+class MyForm(DynamicForm):
+    field1 = StringField(('Field1'),
+        description=('Your field number one!'),
+        validators = [DataRequired()], widget=BS3TextFieldWidget())
+    field2 = StringField(('Field2'),
+        description=('Your field number two!'), widget=BS3TextFieldWidget())
+
 # init Flask
 app = Flask(__name__)
+Bootstrap(app)
+def index():
+    return render_template('methode3.html')
 # Basic config with security for forms and session cookie
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.db')
@@ -17,67 +35,102 @@ appbuilder = AppBuilder(app, db.session)
 
 #creating the views
 class MyView(BaseView):
-    default_view = 'method1'
-    @expose('/method1/')
+########################          ESPACE ADMIN          ############################
+    default_view = 'AccueilAdmin'
+
+    ##########          TABLEAU DE BORD         ############
+    @expose('/AccueilAdmin/')
     @has_access
-    def method1(self):
+    def AccueilAdmin(self):
         # do something with param1
         # and return to previous page or index
-        return 'Hello'
+        
+        self.update_redirect()
+        return self.render_template('tableaudebord.html')
+
+    ##########          FONCTIONNALITÉS         ############
+
     @expose('/method2/<string:param1>')
     @has_access
     def method2(self, param1):
         # do something with param1
         # and render template with param
         param1 = 'Goodbye %s' % (param1)
-        return param1
-    @expose('/methode3/<string:param1>')
+        self.update_redirect()
+        return self.render_template('fonctionnalités.html',
+                            param1 = param1)
+
+
+    @expose('/method100/<string:param1>')
+    @has_access
+    def method100(self, param1):
+        # do something with param1
+        # and render template with param
+        param1 = 'Goodbye %s' % (param1)
+        self.update_redirect()
+        return self.render_template('login.html',
+                            param1 = param1)
+
+
+
+    ##########          CREER UN COMPTE         ############
+    @expose('/creercompte/<string:param1>')
+    @has_access
+    def method4(self, param1):
+        # do something with param1
+        # and render template with param
+        param1 = 'Goodbye %s' % (param1)
+        self.update_redirect()
+        return self.render_template('creercompte.html',
+                            param1 = param1)
+########################          ESPACE UTILISATEUR         ############################
+
+    ##########          ACCUEIL UTILISATEUR        ############
+    view = 'utilisateur'
+    @expose('/index/')
+    @has_access
+    def index(self):
+        # do something with param1
+        # and return to previous page or index
+        
+        self.update_redirect()
+        return self.render_template('login.html')
+
+    ##########          LOGIN UTILISATEUR        ############
+
+    @expose('/Fonctionnalités/<string:param1>')
     @has_access
     def method3(self, param1):
         # do something with param1
         # and render template with param
         param1 = 'Goodbye %s' % (param1)
         self.update_redirect()
-        return self.render_template('methode3.html',
+        return self.render_template('index.html',
                             param1 = param1)
-appbuilder.add_view(MyView, "Method1", category='My View')
-appbuilder.add_link("Method2", href='/myview/method2/john', category='My View')
-appbuilder.add_link("Methode3", href='/myview/methode3/john', category='My View')
 
 
 
-from sqlalchemy import Column, Integer, String, ForeignKey, Date
-from sqlalchemy.orm import relationship
-from flask_appbuilder import Model
+########################          ESPACE ADMIN          ############################
+#accueil admin tableau de bord
+appbuilder.add_view(MyView, "Accueil Admin", category='Espace Admin')
 
-class ContactGroup(Model):
-    id = Column(Integer, primary_key=True)
-    name = Column(String(50), unique = True, nullable=False)
+appbuilder.add_link("Method2", href='/myview/method2/john', category='Espace Admin')
+appbuilder.add_link("Fonctionnalités", href='/myview/Fonctionnalités/john', category='Espace Admin')
+appbuilder.add_link("login form", href='/myview/method100/john', category='Espace Admin')
+appbuilder.add_link("Créer un compte", href='/myview/creercompte/john', category='Espace Admin')
 
-    def __repr__(self):
-        return self.name
+########################          ESPACE UTILISATEUR          ############################
 
-    id = Column(Integer, primary_key=True)
-    name =  Column(String(150), unique = True, nullable=False)
-    address =  Column(String(564), default='Street ')
-    birthday = Column(Date)
-    personal_phone = Column(String(20))
-    personal_cellphone = Column(String(20))
-    contact_group_id = Column(Integer, ForeignKey('contact_group.id'))
-    contact_group = relationship("ContactGroup")
+appbuilder.add_link("index", href='/myview/index/john', category='Espace utilisateur')
 
-    def __repr__(self):
-        return self.name
+########################          ESPACE CLIENT          ############################
+
+appbuilder.add_link("Method2", href='/myview/method2/john', category='Espace Client')
 
 
-from flask_appbuilder import ModelView
-from flask_appbuilder.models.sqla.interface import SQLAInterface
-
-class GroupModelView(ModelView):
-    datamodel = SQLAInterface(ContactGroup)
-    related_views = [ContactModelView]
 
 
+        
 
 
 
