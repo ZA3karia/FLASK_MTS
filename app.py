3e2ad6f1a01f0d35c26bc5835d350bf908e26ca6
Narjiss Sallahi <72303318+SallahiNarjiss@users.noreply.github.com>
2020-10-28 17:29:04 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,13 +1,31 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_appbuilder import SQLA, AppBuilder, AppBuilder, expose, BaseView, has_access
 from flask_bootstrap import Bootstrap
+import folium
 
 from wtforms import Form, StringField
 from wtforms.validators import DataRequired
 from flask_appbuilder.fieldwidgets import BS3TextFieldWidget
 from flask_appbuilder.forms import DynamicForm
 
+import haversine as hs                      #these two import are only necessery during the test, later they will be uneccesery 
+from haversine import Unit
+from algo import cw, vns
+
+from flask_ngrok import run_with_ngrok
+# import map
+Test = True
+coords = []
+nodes = []
+if Test:
+    fake_data = "clt1_rabat Latitude: 34.0128 Longitude: -6.8314    clt2_rabat Latitude: 34.0623 Longitude: -6.7889   clt3_rabat Latitude: 33.9371 Longitude: -6.9028   clt4_rabat Latitude: 33.9354 Longitude: -6.8081   clt5_casa Latitude: 33.5998 Longitude: -7.6321   clt7_casa Latitude: 33.6060 Longitude: -7.5620   clt8_casa Latitude: 33.5872 Longitude: -7.5229   clt9_casa Latitude: 33.5500 Longitude: -7.6891   clt10_casa Latitude: 33.5368 Longitude: -7.6829   clt11_casa Latitude: 33.5895 Longitude: -7.6128   clt12_fez Latitude: 34.0041 Longitude: -5.0350   clt13_fez Latitude: 34.0561 Longitude: -5.0455   clt14_mkech Latitude: 31.6680 Longitude: -8.0228   clt15_mkech Latitude: 31.6516 Longitude: -8.0653   clt16_mkech Latitude: 31.6183 Longitude: -8.0653   clt17_tanger Latitude: 35.7799 Longitude: -5.8059   clt18_tanger Latitude: 35.7613 Longitude: -5.7817   clt19_tanger Latitude: 35.7414 Longitude: -5.7948   clt20_tetouan Latitude: 35.5677 Longitude: -5.4097"
+    A = fake_data.split()
+    Entropot = ('INPT',33.9794, -6.8673)
+    Client=  [ (A[5*i],float(A[5*i+2]),float(A[5*i+4]) ) for i in range(19)]
+    Client
+    INPUT = [(i+1, Entropot[0], Client[i][0],Entropot[1],Entropot[2],Client[i][1],Client[i][2], hs.haversine((Entropot[1],Entropot[2]),(Client[i][1],Client[i][2]),unit=Unit.METERS) ) for i in range(19)]
+    
 
 class MyForm(DynamicForm):
     field1 = StringField(('Field1'),
@@ -19,6 +37,7 @@ class MyForm(DynamicForm):
 # init Flask
 app = Flask(__name__)
 Bootstrap(app)
+run_with_ngrok(app)  # Start ngrok when app is run
 def index():
     return render_template('login.html')
 # Basic config with security for forms and session cookie
@@ -31,6 +50,7 @@ app.config['SECRET_KEY'] = 'thisismyscretkey'
 db = SQLA(app)
 # Init F.A.B.
 appbuilder = AppBuilder(app, db.session)
+
 
 
 #creating the views
@@ -239,9 +259,17 @@ class MyView(BaseView):
     @expose('/OptimisationTrajet/<string:param1>')
     @has_access
     def OptimisationTrajet(self, param1):
-        # do something with param1
-        # and render template with param
-        param1 = 'Goodbye %s' % (param1)
+        if Test:
+            my_input = cw.Distance_matrice(INPUT)
+            my_input.preprocess()
+            my_input.count_saving()
+            my_input.sort()[0][0]
+            my_input.optimise()
+            nodes.append(my_input.display_route())
+            param1 = nodes[0]
+            coords.append(my_input.get_route_coords())
+
+            render_map()
         self.update_redirect()
         return self.render_template('OptimisationTrajet.html',
                             param1 = param1)
@@ -337,6 +365,23 @@ appbuilder.add_link("Historique des expéditions", href='/myview/histexped/john'
 
 
 
+@app.route('/get_map')
+def render_map():
+    m = folium.Map(location=coords[0][0], tiles="OpenStreetMap", zoom_start=6)
+    # Ajout d'un marqueur
+    folium.Marker([34.020882, -6.831650],
+                  popup="Rabat",
+                  icon=folium.Icon(color='green')).add_to(m)
+
+    folium.Marker([31.669746,-7.973328],
+                  popup='Marrakech',
+                  icon=folium.Icon(color='Yellow')).add_to(m)
+    # Ajout d'une ligne à partir de 5 points
+    points = coords[0]
+    folium.PolyLine(points, color="blue",popup=nodes[0] , weight=2.5, opacity=0.8).add_to(m)
+    # return m
+    m.save('templates/map.html')
+    return render_template('map.html')
 
 # Run the development server
-app.run(debug=True)
+app.run()
